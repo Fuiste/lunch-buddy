d4d2dd31edfee27b542ff567f6d465a931979981
Rudy Pelrine <drawncloser@gmail.com>
2021-03-26 09:43:07 -0500

    Immer and action signatures

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,5 +1,79 @@
+import { HangoutState, UserState } from "./state";
+
 export type Noop = {
   type: "NOOP";
 };
 
-export type Action = Noop;
+export type CreateUserRequest = {
+  type: "CREATE_USER_REQUEST";
+  config: {
+    email: string;
+    name: string;
+  };
+};
+
+export type CreateUserSuccess = {
+  type: "CREATE_USER_SUCCESS";
+  user: UserState;
+};
+
+export type CreateUserError = {
+  type: "CREATE_USER_ERROR";
+  error: any;
+};
+
+export type OptInRequest = {
+  type: "OPT_IN_REQUEST";
+};
+
+export type OptInSuccess = {
+  type: "OPT_IN_SUCCESS";
+};
+
+export type OptInError = {
+  type: "OPT_IN_ERROR";
+  error: any;
+};
+
+export type FetchHangoutRequest = {
+  type: "FETCH_HANGOUT_REQUEST";
+};
+
+export type FetchHangoutSuccess = {
+  type: "FETCH_HANGOUT_SUCCESS";
+  hangout: HangoutState | undefined;
+};
+
+export type FetchHangoutError = {
+  type: "FETCH_HANGOUT_ERROR";
+  error: any;
+};
+
+export type FetchHistoryRequest = {
+  type: "FETCH_HISTORY_REQUEST";
+};
+
+export type FetchHistorySuccess = {
+  type: "FETCH_HISTORY_SUCCESS";
+  history: HangoutState[];
+};
+
+export type FetchHistoryError = {
+  type: "FETCH_HISTORY_ERROR";
+  error: any;
+};
+
+export type Action =
+  | Noop
+  | CreateUserError
+  | CreateUserRequest
+  | CreateUserSuccess
+  | OptInError
+  | OptInRequest
+  | OptInSuccess
+  | FetchHangoutError
+  | FetchHangoutRequest
+  | FetchHangoutSuccess
+  | FetchHistoryError
+  | FetchHistoryRequest
+  | FetchHistorySuccess;
