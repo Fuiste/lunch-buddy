38c57b7c5ff7a951cc925dfdc0c3eb37cef05da4
Jonathan Cespedes <jmilescespedes@gmail.com>
2021-03-26 14:52:38 -0400

    a bunch of stuff

--- src/pages/waitingRoom/index.tsx
+++ src/pages/waitingRoom/index.tsx
@@ -2,6 +2,9 @@ import { useHistory } from "react-router";
 import { APP_ROUTES } from "../../router";
 import { HangoutState } from "../../store/state";
 import { WithSession } from "../../util";
+import { Button } from '@material-ui/core'
+
+import './style.scss'
 
 export type WaitingRoomProps = {
   activeHangout: HangoutState | undefined;
@@ -13,20 +16,16 @@ export const WaitingRoom = (props: WaitingRoomProps) => {
   const { activeHangout, session, submitOptIn } = props;
   const { user, optedIn } = session;
 
-  const userWaiting = (
-    <h1 className="waiting-room-pairing__title"> Waiting for a partner...</h1>
-  );
-
-  const userPaired = (<div>
-    <h2>Are you ready to hang?</h2>
-    <button onClick={submitOptIn}>
-      <span>Yes!</span>
-    </button>
-  </div>);
-
-  const content = optedIn ?
-    { userWaiting }
-    : { userPaired };
+  const content = optedIn ? (
+    <h1>Waiting for partner...</h1>
+  ) : (
+      <div>
+        <h2>Ready to hang?</h2>
+        <Button className="waiting-room-button" onClick={submitOptIn}>
+          <span>Yes!</span>
+        </Button>
+      </div>
+    );
 
   if (user === undefined) {
     const frontDoor = APP_ROUTES.frontDoor.toPath();
@@ -38,8 +37,10 @@ export const WaitingRoom = (props: WaitingRoomProps) => {
 
   return (
     <div className="waiting-room-page">
-      <h1 className="waiting-room-title">Waiting Room</h1>
-      {content}
+      <div className="waiting-room-fake-container">
+        <h1>Waiting Room</h1>
+        {content}
+      </div>
     </div>
   );
 };
