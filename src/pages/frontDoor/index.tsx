5129d11fd073ef4d496358c2a7b9715765ed44ae
Rudy Pelrine <drawncloser@gmail.com>
2021-03-26 11:48:34 -0500

    Layout for front door

--- src/pages/frontDoor/index.tsx
+++ src/pages/frontDoor/index.tsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router";
 import { CreateAccountConfig } from "../../api";
 import { APP_ROUTES } from "../../router";
 import { WithSession } from "../../util";
-import logo from "./logo.svg";
 
 export type FrontDoorProps = {
   submitAccountCreate: (config: CreateAccountConfig) => void;
 } & WithSession;
 
 export const FrontDoor = (props: FrontDoorProps) => {
-  const { session } = props;
+  const { session, submitAccountCreate } = props;
   const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
 
   if (session.user !== undefined) {
     const waitingRoom = APP_ROUTES.waitingRoom.toPath();
@@ -19,19 +20,45 @@ export const FrontDoor = (props: FrontDoorProps) => {
 
     return <></>;
   }
+
+  const maybeSubmit = () => {
+    if (email !== "" && name !== "") {
+      submitAccountCreate({ email, name });
+    } else {
+      console.log(
+        `Invalid email and name, got\nEmail: ${email}\nName: ${name}`
+      );
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lunch Buddy</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          maybeSubmit();
+        }}
+      >
+        <label>
+          Email:
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 };
